7094a07907c3786514310f0c9c4c810da5ebda42
bamboosolutions <bamboosolutions@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-26 14:57:32 +0000

    V1.6 Improved handling of ‘sent’ flag.
    git-svn-id: https://plugins.svn.wordpress.org/bamboo-enquiries/tags/1.6@1167778 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ bamboo-enquiries-send.php
@@ -0,0 +1,94 @@
+<?php
+/**************************************************************************************************/
+
+	$to_address    	= $_POST["bamboo_enquiry_form_to_address"];    	// ADDRESS TO SEND ENQUIRIES TO
+	$from_address  	= $_POST["bamboo_enquiry_form_from_address"];	// ADDRESS TO SEND ENQUIRIES FROM
+	$reply_address	= $from_address;                               	// DEFAULT REPLY ADDRESS IF ONE IS NOT SUPPLEID
+	$subject       	= 'Website Enquiry';                           	// START OF THE EMAIL SUBJECT
+	$intro         	= '<p>There has been an enquiry sent from your website, the details are below:</p>'; // INTRO TO THE EMAIL
+
+	// ESTABLISH IF THE FORM IS BLANK
+	$all_blank = true;
+	foreach ( $_POST as $key => $value ) {
+		if ( ( substr( $key, 0, 20) != "bamboo_enquiry_form_" && $key != "undefined" ) && ( $value != '' ) ) {
+			$all_blank = false;
+		}
+	}
+
+	// IF THE FORM ISN'T BLANK WE CAN SEND THE ENQUIRY
+	if( ! $all_blank ) {
+
+		// GENERATE A RANDOM MIME CONTENT BOUNDARY
+		$mime_boundary = uniqid('noodle-enquiries');
+
+		// CONSTRUCT THE HEADERS
+		$headers = "MIME-Version: 1.0" . "\r\n";
+		$headers .= "Content-Type: multipart/mixed;boundary=\"$mime_boundary\"\r\n";
+		$headers .= "From: $from_address" . "\r\n";
+		$headers .= "Reply-To: $reply_address" . "\r\n";
+
+		// CONSTRUCT THE FORM CONTENT
+		$content = "<html><head><title>$subject</title></head><body>$intro";
+		foreach ( $_POST as $key => $value ) {
+			if( substr( $key, 0, 20 ) != "bamboo_enquiry_form_" && $key != "undefined" ) {
+				if( is_array( $value ) ) {
+					$text = '';
+					foreach( $value as $val ) {
+						if( ''!=$text ) {
+							$text.=', ';
+						}
+						$text.= $val;
+					}
+				} else {
+					$text = $value;
+				}
+				$content .= "<p><strong>" . str_replace( "_", " ", $key ) . ":</strong>&nbsp;" . $text . "</p>";
+			}
+		}
+
+		$file_attached = false;
+		foreach ( $_FILES as $key => $value ) {
+			if($_FILES[$key]["size"]>0) {
+				$file_attached = true;
+			}
+		}
+		if( true==$file_attached ) {
+			$content .= "<p><strong>File Attached</strong></p>";
+		}
+
+		$content .= "</body></html>";
+
+		// CONSTRUCT THE MESSAGE
+		$message  = "This is a MIME encoded message.";
+		$message .= "\r\n\r\n--" . $mime_boundary . "\r\n";
+		$message .= "Content-Type: text/html;charset=utf-8\r\n\r\n";
+		$message .= $content;
+		$message .= "\r\n\r\n--" . $mime_boundary . "\r\n";
+
+		// ADD ANY SUBMITTED FILES
+		foreach ( $_FILES as $key => $value ) {
+			if($_FILES[$key]["size"]>0) {
+				$message .= "Content-Type: {" . $_FILES[$key]["type"] . "}; name=\"" . $_FILES[$key]["name"] . "\"\r\n";
+				$message .= "Content-Transfer-Encoding: base64\r\n";
+				$message .= "Content-Disposition: attachment;\r\n; filename=\"" . $_FILES[$key]["name"] . "\"\r\n\r\n";
+				$message .= chunk_split(base64_encode(file_get_contents($_FILES[$key]["tmp_name"])));
+				$message .= "\r\n\r\n--" . $mime_boundary . "\r\n";
+			}
+		}
+
+		// SEND THE MESSAGE
+		mail( $to_address, $subject, $message, $headers );
+
+	}
+
+	// REDIRECT BACK TO CALLING PAGE
+	$refer = $_SERVER['HTTP_REFERER'];
+	if( 0< strpos( $refer, '?' ) ) {
+		$refer.= '&sent';
+	} else {
+		$refer.='?sent';
+	}
+    header("Location: " . $refer);
+
+/**************************************************************************************************/
+?>
---
+++ bamboo-enquiries.js
@@ -0,0 +1,97 @@
+/**************************************************************************************************/
+
+     function queryString()
+     {
+         var vars = [], hash;
+         var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+         for(var i = 0; i < hashes.length; i++)
+         {
+             hash = hashes[i].split('=');
+             vars.push(hash[0]);
+             vars[hash[0]] = hash[1];
+         }
+         return vars;
+     }
+
+/**************************************************************************************************/
+
+	jQuery(document).ready(function(){
+
+		var sent = false;
+		var qs = queryString();
+		for (index = 0; index < qs.length; ++index) {
+    		if( 'sent'===qs[index] ) {
+    			sent = true;
+    		}
+    	}
+		if( sent ) {
+			jQuery('.bamboo_enquiry').empty();
+			jQuery('.bamboo_enquiry').append('<div class="bamboo_enquiry_confirm"><h4>Thank you for your enquiry</h4><h4>We will get back to you as soon as possible</h4></div>');
+		}
+
+		jQuery('.bamboo_enquiry.auto_labels input[type="text"], .bamboo_enquiry.auto_labels textarea').each(function(){
+
+			var input = jQuery(this);
+			var label = input.prev();
+			var prompt = label.html();
+
+			input.val(prompt);
+			label.hide();
+
+			input.blur(function(){
+				if(input.val()==='') {
+					input.val(prompt);
+				}
+			});
+
+			input.focus(function(){
+				if(input.val()===prompt) {
+					input.val('');
+					input.removeClass('error');
+				}
+			});
+
+		});
+
+		jQuery('.bamboo_enquiry').each(function(){
+			jQuery(this).submit(function(){
+
+				jQuery('.bamboo_enquiry.auto_labels input[type="text"], .bamboo_enquiry.auto_labels textarea').each(function(){
+
+					var input = jQuery(this);
+					var label = input.prev();
+					var text = input.val();
+					var prompt = label.html();
+					if (text===prompt) {
+						input.val('');
+						text = '';
+					}
+				});
+
+				jQuery('.bamboo_enquiry input[type="text"], .bamboo_enquiry textarea').each(function(){
+					var input = jQuery(this);
+					var label = input.siblings('label[for="' + input.attr('name') + '"]');
+					var text = input.val();
+					var prompt = label.html();
+					var promptLastChar = prompt.substr(prompt.length-1);
+					if('*'===promptLastChar) {
+						if(''===text) {
+							input.addClass('error');
+						} else {
+							input.removeClass('error');
+						}
+					}
+				});
+
+				if(jQuery('.bamboo_enquiry .error').length>0) {
+					return false;
+				}
+
+				return true;
+
+			});
+		});
+
+	});
+
+/**************************************************************************************************/
